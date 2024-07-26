da14faa6a12aae8fb6176d85f71f215bdc49899f
Anatoly Batov <play.hoffa@gmail.com>
2024-07-26 15:38:29 +0400

    commit

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
-
-date_create_from_format
+debugger
+function name(params) {
+  let counter = 5;
+let sum = 0;
+  for (let i = 0; i < counter + 1; i++) {
+    if (i % 2) {
+      continue;
+      sum = sum + 5;
+    }
+    sum += counter;
+  }
+  
+}
+console.log(sum);
